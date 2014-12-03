2eb755dccf54d85463313133524ccc692c7cdcef
Jon Uland <jon@aesthetly.com>
2014-12-03 10:14:52 -0700

    adds jQuery functionality edits to header

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 $(document).ready( function () {
 	
-	// Scroll Functionality
-	$(window).scroll( function () {
-		if ($(window).scrollTop() < 30) {
-			$('.site-header').slideUp("fast");
-		} else if ($(window).scrollTop() > 30) {
-			$('.site-header').slideDown("fast");
-		}
 
-		if ($(window).scrollTop() > 100) {
+	// Scroll Functionality
+
+	
+	$(window).scroll(function () {
+		$('.site-header').slideDown('fast');
+	});
+	
+
+	
+	$(window).scroll( function () {
+		// Portfolio Items
+		if ($(window).scrollTop() > $(window).height() ) {
 			var delay = 0,
 				increment = 300;
 			$('.portfolio-item').each(function() {
-				$(this).delay(delay).fadeTo('slow', 1);
+				$(this).delay(delay).fadeTo(800, 1);
 				delay += increment;
 			});
 		}
 	});
+
+	
 
 	// Nav-toggle
 	$('.nav-toggle').on("click", function () {
